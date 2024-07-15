ec07f32f706f313b9c3980b7afbffa01b7278dcd
CWJ1222 <qadzcwsx@gmail.com>
2024-07-16 00:06:12 +0900

    make project folder

--- nextLast sql reboot(0.main)/board/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-const HomePage = () => (
-  <div className="flex items-center justify-center min-h-screen">
-    <h1 className="text-4xl">Welcome to playground</h1>
-  </div>
-);
-
-export default HomePage;
--- nextLast sql reboot(0.main)/board/src/pages/signup.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from '../components/Auth/SignUpForm';
-
-const SignUpPage = () => (
-  <div className="flex items-center justify-center min-h-screen">
-    <SignUpForm />
-  </div>
-);
-
-export default SignUpPage;
