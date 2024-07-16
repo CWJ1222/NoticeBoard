0ef6ee685b5e8cb52407908b8bcb864a9b210a11
CWJ1222 <qadzcwsx@gmail.com>
2024-07-16 10:14:38 +0900

    connection error

--- P.NoticeBoard(4.My Info)/board/src/pages/api/board/index.ts
+++ P.NoticeBoard(4.My Info)/board/src/pages/api/board/index.ts
@@ -3,9 +3,11 @@ import connection from '../../../utils/db';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    const [rows] = await connection.query('SELECT * FROM posts ORDER BY created_at DESC');
+    const conn = await connection.getConnection();
+    const [rows] = await conn.query('SELECT * FROM posts ORDER BY created_at DESC');
 
     res.status(200).json(rows);
+    conn.release();
   } else {
     res.status(405).json({ message: 'Method not allowed' });
   }
