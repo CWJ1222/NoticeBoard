03ed731190a4e0f4d0345ceed8725021cc0742ff
seoyoung <mig3023@naver.com>
2024-07-17 13:41:05 +0900

    수정안함

--- P.NoticeBoard(4.My Info)/board/src/pages/api/auth/signup.ts
+++ P.NoticeBoard(4.My Info)/board/src/pages/api/auth/signup.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import connection from '../../../utils/db';
 import { hashPassword } from '../../../utils/auth';
+import connection from '../../../utils/db';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -10,11 +10,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
     const hashedPassword = await hashPassword(password);
     try {
-      await connection.query('INSERT INTO users (email, nickname, password, coin) VALUES (?, ?, ?, ?)', [email, nickname, hashedPassword, 0]);
+      await connection.query(
+        'INSERT INTO users (email, nickname, password, coin) VALUES (?, ?, ?, ?)',
+        [email, nickname, hashedPassword, 0]
+      );
       res.status(201).json({ message: 'User created' });
     } catch (error: unknown) {
-      const errorMessage = error instanceof Error ? error.message : 'An unexpected error occurred';
-      res.status(500).json({ message: 'Internal Server Error', error: errorMessage });
+      const errorMessage =
+        error instanceof Error ? error.message : 'An unexpected error occurred';
+      res
+        .status(500)
+        .json({ message: 'Internal Server Error', error: errorMessage });
     }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
