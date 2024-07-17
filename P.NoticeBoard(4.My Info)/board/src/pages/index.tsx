a38c1d6a0f0f3ff4e92cb9e2b8a27c5921287859
CWJ1222 <qadzcwsx@gmail.com>
2024-07-17 14:38:15 +0900

    qr insert

--- P.NoticeBoard(4.My Info)/board/src/pages/index.tsx
+++ P.NoticeBoard(4.My Info)/board/src/pages/index.tsx
@@ -1,6 +1,12 @@
+import Image from 'next/image';
+import qrCodeImage from '../../public/adobe-express-qr-code.png';
+
 const HomePage = () => (
-  <div className="flex items-center justify-center min-h-screen">
-    <h1 className="text-4xl">Welcome to playground</h1>
+  <div className="flex flex-col items-center justify-center min-h-screen">
+    <h1 className="text-4xl">Welcome to NoticeBoard</h1>
+    <div className="mt-20">
+      <Image src={qrCodeImage} alt="QR Code" width={200} height={200} />
+    </div>
   </div>
 );
 
